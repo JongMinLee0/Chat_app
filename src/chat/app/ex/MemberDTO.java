73a69c88e588ad6911fddf50c81214fd71474865
JongMinLee <1393062@naver.com>
2019-05-15 21:57:59 +0900

    add : Create MemberDTO

---
+++ src/chat/app/ex/MemberDTO.java
@@ -0,0 +1,29 @@
+package chat.app.ex;
+
+public class MemberDTO {
+
+	private String email;
+	private String pwd;
+	private String nick;
+	
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getPwd() {
+		return pwd;
+	}
+	public void setPwd(String pwd) {
+		this.pwd = pwd;
+	}
+	public String getNick() {
+		return nick;
+	}
+	public void setNick(String nick) {
+		this.nick = nick;
+	}
+	
+	
+}
